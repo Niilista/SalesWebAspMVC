cd22d5a0d81ec3883055e4bc81ae7d9eb1dcde1f
Victor Hugo <victorhugo.vhs@hotmail.com>
2020-05-05 18:27:11 -0300

    First controller and Razor pages test

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace SalesWebMvc.Controllers
             return View();
         }
 
+        public IActionResult Ola()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
